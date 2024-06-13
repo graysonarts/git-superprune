87be4c146d95da95be9527d5ac10c37ef6a94bf1
Grayson <russell.hay@gmail.com>
2024-06-13 15:34:09 -0700

    initial feature complete

---
+++ src/branch.rs
@@ -0,0 +1,169 @@
+use git2::{BranchType, Oid, Repository};
+
+#[derive(Debug)]
+pub struct Branch {
+    pub name: String,
+    pub commit: String,
+    pub gone: bool,
+    pub current: bool,
+}
+
+pub trait BranchList {
+    fn get_branches(&self) -> Vec<Branch>;
+}
+
+impl BranchList for Repository {
+    fn get_branches(&self) -> Vec<Branch> {
+        let branches = self.branches(None).unwrap();
+        let mut output = Vec::new();
+        for branch in branches
+            .filter_map(|b| b.ok())
+            .filter(|(_, b_type)| *b_type == BranchType::Local)
+        {
+            let (branch, _branch_type) = branch;
+            let current = branch.is_head();
+            let commit = branch.get().peel_to_commit();
+            let commit: Oid = match commit {
+                Ok(c) => c.id(),
+                Err(e) => {
+                    println!("Error getting commit: {:?}", e);
+                    continue;
+                }
+            };
+            let upstream = branch.upstream();
+            let gone = match upstream {
+                Ok(_) => false,
+                Err(e) => match e.class() {
+                    git2::ErrorClass::Reference => true,
+                    _ => false,
+                },
+            };
+            let name = branch.name();
+            if name.is_err() {
+                println!("Error getting branch name: {:?}", name);
+                continue;
+            }
+            let name = name.unwrap();
+            if name.is_none() {
+                continue;
+            }
+            let name = name.unwrap();
+
+            let branch = Branch {
+                name: name.to_string(),
+                commit: commit.to_string().as_str()[0..8].to_string(),
+                gone,
+                current,
+            };
+            output.push(branch);
+        }
+        output
+    }
+}
+
+pub fn get_branches(repo: &dyn BranchList) -> Vec<Branch> {
+    repo.get_branches()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockBranchList;
+
+    impl BranchList for MockBranchList {
+        fn get_branches(&self) -> Vec<Branch> {
+            vec![
+                Branch {
+                    name: "master".to_string(),
+                    commit: "todo".to_string(),
+                    gone: false,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/1".to_string(),
+                    commit: "todo".to_string(),
+                    gone: false,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/2".to_string(),
+                    commit: "todo".to_string(),
+                    gone: false,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/3".to_string(),
+                    commit: "todo".to_string(),
+                    gone: false,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/4".to_string(),
+                    commit: "todo".to_string(),
+                    gone: false,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/5".to_string(),
+                    commit: "todo".to_string(),
+                    gone: true,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/6".to_string(),
+                    commit: "todo".to_string(),
+                    gone: true,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/7".to_string(),
+                    commit: "todo".to_string(),
+                    gone: true,
+                    current: false,
+                },
+                Branch {
+                    name: "feature/8".to_string(),
+                    commit: "todo".to_string(),
+                    gone: true,
+                    current: false,
+                },
+            ]
+        }
+    }
+
+    #[test]
+    fn test_get_branches() {
+        let branches = get_branches(&MockBranchList);
+        assert_eq!(branches.len(), 9);
+        let gone_branch_count = branches.iter().filter(|b| b.gone).count();
+        assert_eq!(gone_branch_count, 4);
+    }
+
+    #[test]
+    fn repository_branch_output() {
+        let repo = Repository::open("../elka").expect("Unable to open repository");
+        let branches = repo.get_branches();
+        assert_eq!(branches.len(), 6);
+        let gone_branch_count = branches.iter().filter(|b| b.gone).count();
+        assert_eq!(gone_branch_count, 2);
+    }
+
+    #[test]
+    fn can_read_main_branch() {
+        let repo = Repository::open("../elka").expect("Unable to open repository");
+        let branches = repo.get_branches();
+        let main_branch = branches.iter().find(|b| b.current);
+        assert!(main_branch.is_some());
+        assert_eq!(main_branch.unwrap().name.as_str(), "main");
+    }
+
+    #[test]
+    fn can_read_commit() {
+        let repo = Repository::open("../elka").expect("Unable to open repository");
+        let branches = repo.get_branches();
+        let main_branch = branches.iter().find(|b| b.current);
+        assert!(main_branch.is_some());
+        assert_eq!(main_branch.unwrap().commit.as_str(), "8dcade56");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,95 @@
-use clap::Parser;
-use git_version::git_version;
+use std::sync::atomic::AtomicU8;
 
-#[derive(Debug, Parser)]
-struct Args {}
+use branch::BranchList;
+use clap::Parser;
+use color_eyre::Result;
+use const_format::formatcp;
+use git2::Repository;
+use git_version::git_version;
+use tracing::{debug, info};
+use tracing_subscriber::field::debug;
+
+mod branch;
 
 const GIT_VERSION: &str = git_version!();
+const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
+const VERSION: &str = formatcp!("v{CRATE_VERSION} ({GIT_VERSION})");
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Debug, Parser)]
+#[clap(
+    name = "git-superprune",
+    version = VERSION,
+    about = "Prune local branches that no longer exist on the remote",
+)]
+struct Args {
+    #[clap(short, long, default_value = "false")]
+    /// Display verbose output
+    verbose: bool,
+
+    #[clap(short('x'), long, default_value = "false")]
+    /// Execute the branch deletion
+    execute: bool,
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
     let args = Args::parse();
-    println!("{:?}", args);
+
+    let format = tracing_subscriber::fmt::format()
+        .without_time()
+        .with_target(false);
+
+    tracing_subscriber::fmt()
+        .with_max_level(if args.verbose {
+            tracing::Level::DEBUG
+        } else {
+            tracing::Level::INFO
+        })
+        .event_format(format)
+        .init();
+
+    debug!("Scanning Repository");
+
+    let dry_run = !args.execute;
+    let deleted = AtomicU8::new(0);
+
+    let repository = Repository::discover(".")?;
+    let branches = repository
+        .get_branches()
+        .into_iter()
+        .map(|b| {
+            debug!("Checking branch: {}", b.name);
+            b
+        })
+        .filter(|b| b.gone);
+    for branch in branches {
+        if dry_run {
+            info!("Would delete branch: {}", branch.name);
+            deleted.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        } else {
+            let mut git_branch = repository.find_branch(&branch.name, git2::BranchType::Local)?;
+            git_branch.delete()?;
+            info!("Deleted branch: {}", branch.name);
+            deleted.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        }
+    }
+
+    if deleted.load(std::sync::atomic::Ordering::Relaxed) == 0 {
+        info!("No branches to prune");
+        return Ok(());
+    }
+
+    if dry_run {
+        info!(
+            "Would have deleted {} branches, rerun with -x to execute",
+            deleted.load(std::sync::atomic::Ordering::Relaxed)
+        );
+    } else {
+        info!(
+            "Pruned {} branches",
+            deleted.load(std::sync::atomic::Ordering::Relaxed)
+        );
+    }
+
     Ok(())
 }
